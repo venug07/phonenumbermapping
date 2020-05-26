655290af18eceb7ddd18cef8b8e68a41d1354f6e
venugopal <venug078175@yahoo.com>
2020-05-26 18:02:24 +0530

    Mapping words to string done

--- lib/mapping.rb
+++ lib/mapping.rb
@@ -1,5 +1,7 @@
 class Mapping
+  attr_reader :phonenumber
 
+#Assigning phone number variables and mapping to words
     def initialize(phonenumber)
        @phonenumber = phonenumber
        @mapping_strings = { '2' => %w[a b c],
@@ -12,10 +14,11 @@ class Mapping
        	                    '9' => %w[w x y z]
 
                           }
-      @mapwords = []                    
+      @convertedwords = []                    
 
     end
 
+#Validating phone number empty or having 10 digits
     def validate_phone_number?
        if phonenumber.nil?
        	return false
@@ -25,6 +28,8 @@ class Mapping
        end
     end 
 
+
+#Getting words from dictionary
     def get_dictionary_words
        @dictionary_words = []
        File.foreach(Dir.pwd + '/dictionary/dictionary.txt') do |dic_word|
@@ -32,21 +37,32 @@ class Mapping
        end 
     end  	
 
+#Converting phone number to words
+#Dividing length wise and getting combinations
     def convert_to_words
-      return "Invalid Phone Number" unless validate_phone_number?
+      return "Invalid Phone Number, Please enter again" unless validate_phone_number?
 
       get_dictionary_words
-      tot_length = @phonenumber.length - 3
+      tot_length = @phonenumber.length
       min_length = 2
       @mapwords = @phonenumber.chars.map { |phno| @mapping_strings[phno] }
-      while min_length < tot_length
-         fstphnoarray = @mapwords[0..i]
+      while min_length < (tot_length - 3)
+         fstphnoarray = @mapwords[0..min_length]
          secphnoarray = @mapwords[(min_length + 1)..(tot_length - 1)]
-         get_mapping_words([fstphnoarray,secphnoarray]) 
+         get_mapping_words([fstphnoarray, secphnoarray]) 
          min_length += 1  
       end	
+      get_mapping_words([@mapwords[0..2], @mapwords[3..5], @mapwords[6..9]])
+      get_mapping_words([@mapwords[0..2], @mapwords[3..6], @mapwords[7..9]])
+      get_mapping_words([@mapwords[0..3], @mapwords[4..6], @mapwords[7..9]])
+
+      @convertedwords << (@mapwords.shift.product(*@mapwords).map(&:join) & @dictionary_words).join(', ')
+      @convertedwords.uniq!
+      @convertedwords.reject! { |phn| phn.join.length < 10 if phn.is_a?(Array) || phn.length < 10 }
+      @convertedwords
     end	
 
+#Getting matching words from dictionary for all combinations
    def get_mapping_words(phnowordsarrys)
       matchwords = []
       phnowordsarrys.each do |phnowords|
@@ -58,10 +74,17 @@ class Mapping
       return if matchwords.any?(&:empty?)
 
       if matchwords.size == 2
-        @mapwords += matchwords[0].product(matches[1])
+        @convertedwords += matchwords[0].product(matchwords[1])
       elsif matchwords.size == 3
-      	@mapwords += matches[0].product(matches[1]).product(matches[2]).map(&:flatten)
-      end  	 		 	
-   end	
+      	@convertedwords += matchwords[0].product(matchwords[1]).product(matchwords[2]).map(&:flatten)
+      end	 		 	
+   end
+
+
 
 end
+
+puts 'Please enter the 10 digit phone number :'
+phonenumber = gets.chomp
+mapphno = Mapping.new(phonenumber)
+puts mapphno.convert_to_words
